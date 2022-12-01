e4300bc73f5d1f01184d60f89a858104290db350
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 19:10:34 -0300

    refactoring user router

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { errorHandlerWrapper } from '../middlewares/errorHandlerWrapper';
+import { userValidator } from '../middlewares/userValidator';
 import { SignUpController } from '../use-cases/SignUp/SignUpController';
 
 const router = Router();
 
 const signUpController = new SignUpController();
 
-router.post('/', errorHandlerWrapper((res, req) => signUpController.handle(res, req)));
+router.post('/', userValidator, errorHandlerWrapper(signUpController.handle));
 
 export default router;
