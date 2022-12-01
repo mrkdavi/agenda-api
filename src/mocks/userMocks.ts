827b2555a5720ee420446014402e9f46994e3271
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 19:09:37 -0300

    creating mocks

---
+++ src/mocks/userMocks.ts
@@ -0,0 +1,16 @@
+export const userMock = {
+  id: 'cjld2cjxh0000qzrmn831i7rn',
+  name: 'John Doe',
+  email: 'Johndoe@gmail.com',
+  password: '123456'
+};
+
+export const signUpRequestMock = {
+  name: 'John Doe',
+  email: 'Johndoe@gmail.com',
+  password: '123456'
+};
+
+export const userTokenMock = {
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+};
