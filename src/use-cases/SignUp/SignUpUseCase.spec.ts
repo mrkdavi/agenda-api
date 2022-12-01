52a62e24879cdf39e821910a4212997498919261
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 19:12:46 -0300

    creating sign up unit test

---
+++ src/use-cases/SignUp/SignUpUseCase.spec.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { User } from '../../entities/User/User';
+import { SignUpUseCase } from '../../use-cases/SignUp/SignUpUseCase';
+import { signUpRequestMock, userMock, userTokenMock } from '../../mocks/userMocks';
+import { IUserRepository } from '../../@types/repositories/IUserRepository';
+import * as generateToken from '../../helpers/generateToken';
+
+describe('User', () => {
+  const userRepository = {} as IUserRepository;
+  const signUpUseCase = new SignUpUseCase(userRepository);
+  const user = new User(userMock);
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it('should response a token', () => {
+    userRepository.create = vi.fn().mockResolvedValueOnce(user);
+    vi.spyOn(generateToken, 'default').mockReturnValueOnce(userTokenMock);
+
+    const response = signUpUseCase.execute(signUpRequestMock);
+
+    expect(response).resolves.toEqual(expect.objectContaining({
+      token: expect.any(String)
+    }));
+  });
+});
