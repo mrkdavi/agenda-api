2d34d26846fd11ec29229476c584a6aab373b0cc
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 19:08:46 -0300

    refactoring entities

--- src/entities/User/User.spec.ts
+++ src/entities/User/User.spec.ts
@@ -1,35 +1,30 @@
 import { describe, expect, it, test } from 'vitest';
 import { User } from './User';
+import { userMock } from '../../mocks/userMocks';
 
 describe('User', () => {
-  const userData = {
-    id: 'cjld2cjxh0000qzrmn831i7rn',
-    name: 'John Doe',
-    email: 'Johndoe@gmail.com',
-    password: '123456'
-  };
-  const user = new User(userData);
+  const user = new User(userMock);
 
   it('should create a user', () => {
     expect(user).toEqual(
       expect.objectContaining({
-        props: userData
+        props: userMock
       })
     );
   });
 
   test('if user getters are working', () => {
-    expect(user.id).toEqual(userData.id);
-    expect(user.email).toEqual(userData.email);
-    expect(user.name).toEqual(userData.name);
-    expect(user.password).toEqual(userData.password);
+    expect(user.id).toEqual(userMock.id);
+    expect(user.email).toEqual(userMock.email);
+    expect(user.name).toEqual(userMock.name);
+    expect(user.password).toEqual(userMock.password);
   });
+
   test('if user setters are working', () => {
     const newUserData = {
-      password: '123'
-    }
+      password: userMock.password
+    };
     user.password = newUserData.password;
-
     expect(user.password).toEqual(newUserData.password);
   });
-})
+});
