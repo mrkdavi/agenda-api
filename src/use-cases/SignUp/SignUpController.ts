001eac620148874917ef7fe8e1b1617be7e53c6b
Marcus Queiros <marcusdavi.soar@gmail.com>
2022-12-01 19:11:59 -0300

    refactoring sign up use-case

--- src/use-cases/SignUp/SignUpController.ts
+++ src/use-cases/SignUp/SignUpController.ts
@@ -1,10 +1,13 @@
+import { prisma } from '../../lib/prisma';
 import { Request, Response } from 'express';
 import { StatusCode } from '../../@types/errors/StatusCode';
 import { SignUpUseCase } from './SignUpUseCase';
 
 export class SignUpController {
+  constructor (private signUpUseCase = new SignUpUseCase(prisma.user)) {}
+
   async handle (req: Request, res: Response) {
-    const token = await new SignUpUseCase().execute(req.body);
+    const token = await this.signUpUseCase.execute(req.body);
     res.status(StatusCode.CREATED).json(token);
   }
 }
--- src/use-cases/SignUp/SignUpUseCase.ts
+++ src/use-cases/SignUp/SignUpUseCase.ts
@@ -1,7 +1,20 @@
-import { User } from '../../domain/entities/User/User';
+import { UserRequestData } from '../../@types/auth/UserDTO';
+import generateToken from '../../helpers/generateToken';
+import { IUserRepository } from '../../@types/repositories/IUserRepository';
 
 export class SignUpUseCase {
-  async execute (userData: User) {
-    return userData;
+  constructor (private User: IUserRepository) {}
+  async execute ({ name, email, password }: UserRequestData) {
+    const user = await this.User.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    });
+
+    const token = generateToken({ sub: user.id });
+
+    return token;
   }
 }
